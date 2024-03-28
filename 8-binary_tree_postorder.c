588e59e2f39703f0f7021eee3207142ab899ceff
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-03-28 02:45:18 +0000

    fixed a few issues with task 6, 7, and 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,7 +11,7 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree && !func)
+	if (!tree || !func)
 		return;
 
 	binary_tree_postorder(tree->left, func);
